3429a1b543208adf95e60c89477a4219a5a366a3
Patricia Lor <patricia.lor@gmail.com>
2014-04-25 00:58:37 +1000

    Add header and footer sections to the .sty.

--- makesty.py
+++ makesty.py
@@ -4,7 +4,24 @@ import re
 INPUT_FILE = 'htmlfontawesome.txt'
 OUTPUT_FILE = 'fontawesome.sty'
 
+OUTPUT_HEADER = r'''
+% Identify this package.
+\NeedsTeXFormat{LaTeX2e}
+\ProvidesPackage{fontawesome}[2014/04/24 v4.0.3 font awesome icons]
+
+% Requirements to use.
+\usepackage{fontspec}
+
+% Define shortcut to load the Font Awesome font.
+\newfontfamily{\FA}{FontAwesome}
+% Generic command displaying an icon by its name.
+\newcommand*{\faicon}[1]{{
+  \FA\csname faicon@#1\endcsname
+}}
+'''
+
 with open(INPUT_FILE) as r, open(OUTPUT_FILE, 'w') as w:
+  print(OUTPUT_HEADER, file=w)
   for line in r:
     # Expects to find 'fa-NAME' ending with "
     name = re.findall(r'fa-[^""]*', line)[0]
@@ -20,3 +37,4 @@ with open(INPUT_FILE) as r, open(OUTPUT_FILE, 'w') as w:
           '{{\symbol{{"{symbol}}}}} \def\{camel_name} '
           '{{{{\FA\csname faicon@{name}\endcsname}}}}'.format(name=name,
             camel_name=camel_name, symbol=symbol), file=w)
+  print(r'\endinput', file=w)
